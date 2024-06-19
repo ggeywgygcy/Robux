99e545e7a327ede7e5a4684d591c928fc2c51b33
ggeywgygcy <wasilij18bus@gmail.com>
2024-06-19 15:35:35 +0300

    все готово, надо будет доработать кое-что и финал!

--- robux.js
+++ robux.js
@@ -2,41 +2,14 @@ const nickName = document.getElementById('exampleInputEmail1')
 const robUx = document.getElementById('exampleInputPassword1')
 const age = document.getElementById('exampleInputPassword2')
 const bTn = document.getElementById('rb')
+
 const vivEl = 'Робаксы успешно были отправлены на ваш аккаунт! Они появятся на вашем аккаунте через 24 часа!'
 
 bTn.onclick = function(){
-
-    iffer()
-
-    if (age.value > 18){
-        alert('Похоже вы ошиблись при написании данных своего аккаунта! Попробуйте ввести более корректные данные!')
-    }
-
-    if (robUx.value > 100) {
-        alert('Похоже вы ошиблись при написании данных своего аккаунта или значения! Попробуйте ввести более корректные данные!')
-    }
-
-    alert(vivEl)
-
-    // vivElement(vivEl)
-
-}
-
-function iffer(){
-    if (nickName.value == ''){
-        alert('Вы ввели не все данные своего акаунта! Введите все, чтобы отправить робаксы! ')
-    } if (age.value == ''){
-        alert('Вы ввели не все данные своего акаунта! Введите все, чтобы отправить робаксы!')
-    } if (robUx.value == ''){
-        alert('Вы ввели не все данные своего акаунта! Введите все, чтобы отправить робаксы!')
+    if ((nickName.value == '') ||  (age.value == '') || (robUx.value == '') || age.value > 18 || robUx.value > 100 ){
+        alert('Вы ввели не все данные своего акаунта или ошиблись при их записи! Перепроверьте все, чтобы отправить робаксы! ')
+    }else{
+        alert(vivEl)
     } 
 }
-
-// function vivElement(title){
-
-//     return`
-//     <div id="emailHelp" class="form-text">${title}</div>
-//     `
-
-// }
 
